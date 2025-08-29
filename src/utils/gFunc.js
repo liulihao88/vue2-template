7ec077e680a6cef491200a117f389414fffa731d
liulihao88 <551772063@qq.com>
2025-08-29 11:39:44 +0800

    fix: table黑色主题

--- src/utils/gFunc.js
+++ src/utils/gFunc.js
@@ -1,3 +1,4 @@
+import { cloneDeep } from "lodash-es";
 export function sleep(delay = 0, fn) {
   return new Promise((resolve) =>
     setTimeout(() => {
@@ -5,4 +6,45 @@ export function sleep(delay = 0, fn) {
       resolve();
     }, delay)
   );
+}
+
+/**
+ * 判断传入参数的类型
+ * @param {*} type
+ * getType(new RegExp()) regexp
+ * getType(new Date()) date
+ * getType([]) array
+ * getType({}) object
+ * getType(null) null
+ * getType(123) number
+ */
+export function getType(type) {
+  if (typeof type === "object") {
+    const objType = Object.prototype.toString.call(type).slice(8, -1).toLowerCase();
+    return objType;
+  } else {
+    return typeof type;
+  }
+}
+
+/**
+ * 克隆数据并根据需要复制数组
+ * @param {any} data - 要克隆的数据
+ * @param {number} [times=1] - 如果是数组，要复制的次数
+ * @returns {any} - 克隆后的数据或复制后的数组
+ * clone(123) => 123
+ * clone([1,2, {name: 'andy'}], 2) => [1, 2, {name: 'andy'}, 1, 2, {name: 'andy'}]
+ */
+export function clone(data, times = 1) {
+  // Check if the data is not an array
+  if (getType(data) !== "array") {
+    // If not an array, return a deep clone of the data
+    return cloneDeep(data);
+  }
+  const clonedData = cloneDeep(data);
+  const result = [];
+  for (let i = 0; i < times; i++) {
+    result.push(...clonedData);
+  }
+  return result;
 }
--- src/views/element/tableBlack.vue
+++ src/views/element/tableBlack.vue
@@ -1,37 +1,42 @@
 <template>
   <div class="my-table-container">
-    <el-table :data="list" border fit size="snall" align="center">
-      <el-table-column label="标准名称" prop="standardId">
-        <template slot-scope="scope">
-          {{ scope.$index }}
-        </template>
-      </el-table-column>
-      <el-table-column prop="auditContent" label="审查详情">
-        <template slot-scope="scope">
-          <i class="el-icon-time" />
-          <span>{{ scope.row.display_time }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" prop="created_at" label="操作" width="60">
-        <template slot-scope="scope">
-          <el-dropdown>
-            <span class=""><i class="el-icon-more"></i></span>
-            <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item>
-                <el-button type="text" @click="handleUpdate(scope.row)" icon="el-icon-edit">编辑</el-button>
-              </el-dropdown-item>
-              <el-dropdown-item>
-                <el-button type="text" @click="handleDelete(scope.row)" icon="el-icon-delete">删除</el-button>
-              </el-dropdown-item>
-            </el-dropdown-menu>
-          </el-dropdown>
-        </template>
-      </el-table-column>
-    </el-table>
+    <el-scrollbar>
+      <el-table :data="list" border fit size="snall" align="center">
+        <el-table-column label="标准名称" prop="standardId">
+          <template slot-scope="scope">
+            {{ scope.$index }}
+          </template>
+        </el-table-column>
+        <el-table-column prop="auditContent" label="审查详情">
+          <template slot-scope="scope">
+            <i class="el-icon-time" />
+            <span>{{ scope.row.display_time }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" prop="created_at" label="操作" width="60">
+          <template slot-scope="scope">
+            <div class="dropdown-black">
+              <el-dropdown trigger="click">
+                <span class=""><i class="el-icon-more"></i></span>
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item>
+                    <el-button type="text" @click="handleUpdate(scope.row)" icon="el-icon-edit">编辑</el-button>
+                  </el-dropdown-item>
+                  <el-dropdown-item>
+                    <el-button type="text" @click="handleDelete(scope.row)" icon="el-icon-delete">删除</el-button>
+                  </el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-scrollbar>
   </div>
 </template>
 
 <script>
+import { clone } from "@/utils/gFunc";
 export default {
   data() {
     return {
@@ -56,7 +61,7 @@ export default {
     };
   },
   created() {
-    
+    this.list = clone(this.list, 10);
   },
   methods: {}
 };
@@ -64,7 +69,13 @@ export default {
 <style lang="scss" scoped>
 .my-table-container {
   background: #000;
-  height: 100vh;
+  height: calc(100vh - 55px);
+  overflow: auto;
+  /* 针对 el-table 内部的 el-dropdown 菜单项悬停 */
+  .el-table .el-dropdown-menu__item:hover {
+    background-color: black !important;
+    color: white !important; /* 可选：提高可读性 */
+  }
   /* 表格整体样式 */
   ::v-deep .el-table {
     background-color: #000;
@@ -111,8 +122,8 @@ export default {
       color: #fff;
 
       &:hover {
-        background-color: #333;
-        color: #fff;
+        background-color: #333 !important;
+        color: #fff !important;
       }
     }
   }
@@ -120,8 +131,18 @@ export default {
     background-color: #1a1a1a;
     border: 1px solid #333;
   }
+  .el-dropdown-menu__item:hover {
+    background-color: black !important;
+    color: white !important; /* 可选：文字颜色改为白色提高对比度 */
+  }
   .el-table__row.selected {
     background-color: #000 !important;
+  }
+  .el-popper[x-placement^="bottom"] .popper__arrow::after {
+    border-bottom-color: black !important;
+  }
+  .el-popper[x-placement^="top"] .popper__arrow::after {
+    border-top-color: black !important;
   }
 
   /* 下拉按钮样式 */
@@ -136,5 +157,20 @@ export default {
   &:hover {
     background-color: #1a1a1a;
   }
+}
+
+.el-dropdown-menu__item:hover {
+  background-color: black !important;
+  color: white !important; /* 可选：提高可读性 */
+}
+.el-popper__arrow:before {
+  background-color: black !important;
+  right: 0;
+}
+.popper_arrow::after {
+  border-bottom-color: #333 !important;
+}
+.el-popper ::v-deep .popper__arrow::after {
+  border-bottom-color: #333 !important;
 }
 </style>
