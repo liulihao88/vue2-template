4fe25c0f9b9c1852bf2146e2b09bf5efc078f613
liulihao <551772063@qq.com>
2025-10-13 14:06:49 +0800

    fix: 坐标

--- src/views/three/completeThreePage7.vue
+++ src/views/three/completeThreePage7.vue
@@ -11,7 +11,7 @@
 
     <el-dialog
       :visible.sync="modelLoaded"
-      :title="hoverCoords"
+      :title="hoverCoordsTitle"
       append-to-body
       :close-on-click-modal="false"
       fullscreen
@@ -95,6 +95,13 @@ export default {
     NodeItem,
   },
   computed: {
+    hoverCoordsTitle() {
+      if (this.hoverCoords.visible) {
+        return `坐标: X: ${this.hoverCoords.x} Y: ${this.hoverCoords.y} Z: ${this.hoverCoords.z}`
+      } else {
+        return 'glb文件'
+      }
+    },
     knovaCanvasRef() {
       // 从最外层开始，一步一步判断，确保每一步都存在
       const uploadFileComp = this.$refs.uploadFileRef
